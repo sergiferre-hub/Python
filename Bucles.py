f8f1fb935f59a826f2e3a9aec83ea3d354cf5f8b
sergiferre-hub <sergiferre@iesmontsia.org>
2025-11-07 17:28:46 +0100

    #2 arreglat

--- Bucles.py
+++ Bucles.py
@@ -11,15 +11,16 @@ for i in range(2, 201, 2):
 El_Deu = False
 nota = int(input("Introdueix una nota entre l'1 i el 10 o -1 per acabar: "))
 
-while nota == 10:
-    El_Deu = True
-    print("Hi ha un 10")
+while nota != -1:
+    if nota == 10:
+        El_Deu = True
+        print("Hi ha un 10")
+    else:
+        print("No hi ha cap 10")
     nota = int(input("Introdueix una nota entre l'1 i el 10 o -1 per acabar: "))
+if not El_Deu:
+    print("No hi ha hagut cap 10")
 else:
-    print("No hi ha cap 10")
-    while nota != -1 and not El_Deu:
-     print("No hi ha cap 10")
-     nota = int(input("Introdueix una nota entre l'1 i el 10 o -1 per acabar: "))
+    print("Hi ha hagut un 10")
 
-while nota == -1:
-    print("Fi del programa")
+#3
